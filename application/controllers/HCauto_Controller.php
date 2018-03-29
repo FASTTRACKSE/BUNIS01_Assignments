78aa3d0c253e20c3c2c530a3be1c1f4fb46a4ec9
JStewart77 <37789045+JStewart77@users.noreply.github.com>
2018-03-29 14:58:05 +0700

    change
    controller and model

--- application/controllers/HCauto_Controller.php
+++ application/controllers/HCauto_Controller.php
@@ -8,15 +8,157 @@ class HCauto_Controller extends CI_Controller{
 		$this->load->model('servicesModel');
 	}
 
+//global
+	public function insert(){
+		$data = array(
+			"name" =>$this->input->post("fName"),
+			"description" =>$this->input->post("fDesc"),
+			"tag" =>$this->input->post("fTag")
+		);
+		$this->servicesModel->insert_data($data);
+		redirect("#");
+	}
+
+	public function delete($deleteID){;
+		$this->servicesModel->delete_Data_by_ID($deleteID);
+		redirect("#");
+	}
+
+	public function update($updateID){
+		$data["getUpdateData"] = $this->servicesModel->get_Data($studentID);
+		$data = array(
+			"name" =>$this->input->post("fName"),
+			"description" =>$this->input->post("fDesc"),
+			"tag" =>$this->input->post("fTag")
+		);
+		$this->servicesModel->update_Data_by_ID($updateID,$data);
+		redirect("#");
+	}
+
+// view index
 	public function view(){
 		$data = array(
 			'headtitle' => 'Services',
 		);
-
 		$this->load->view('templates/head');
 		$this->load->view('templates/navbar');
 		$this->load->view('services');
 		$this->load->view('templates/footer');
+	}
+
+// body index
+	public function showBody(){
+		$data = array(
+			'headtitle' => 'Body',
+		);
+		$data["getData"] = $this->servicesModel->get_Body();
+		$this->load->view('templates/head');
+		$this->load->view('templates/navbar');
+		$this->load->view('bodyIndex',$data);
+		$this->load->view('templates/footer');
+	}
+
+	public function showBodyDetail($bodyID){
+		$data = array(
+			'headtitle' => 'Body',
+		);
+		$data["getDataByID"] = $this->servicesModel->get_Data_by_ID($bodyID);
+		// $this->load->view('templates/head');
+		// $this->load->view('templates/navbar');
+		$this->load->view("detailIndex",$data);
+		// $this->load->view('templates/footer');
+	}
+
+//car rescue index
+	public function showCarRescue(){
+		$data = array(
+			'headtitle' => 'Body',
+		);
+		$data["getData"] = $this->servicesModel->get_CarRescue();
+		$this->load->view('templates/head');
+		$this->load->view('templates/navbar');
+		$this->load->view("carRescueIndex",$data);
+		$this->load->view('templates/footer');
+	}
+
+	public function showCarRescueDetail($rescueID){
+		$data = array(
+			'headtitle' => 'Body',
+		);
+		$data["getDataByID"] = $this->servicesModel->get_Data_by_ID($rescueID);
+		// $this->load->view('templates/head');
+		// $this->load->view('templates/navbar');
+		$this->load->view("detailIndex",$data);
+		// $this->load->view('templates/footer');
+	}
+
+//d2d index
+	public function showD2D(){
+		$data = array(
+			'headtitle' => 'Body',
+		);
+		$data["getData"] = $this->servicesModel->get_D2D();
+		$this->load->view('templates/head');
+		$this->load->view('templates/navbar');
+		$this->load->view("d2dIndex",$data);
+		$this->load->view('templates/footer');
+	}
+
+	public function showD2DDetail($d2dID){
+		$data = array(
+			'headtitle' => 'Body',
+		);
+		$data["getDataByID"] = $this->servicesModel->get_Data_by_ID($d2dID);
+		// $this->load->view('templates/head');
+		// $this->load->view('templates/navbar');
+		$this->load->view("detailIndex",$data);
+		// $this->load->view('templates/footer');
+	}
+
+//electrical index
+	public function showElectrical(){
+		$data = array(
+			'headtitle' => 'Body',
+		);
+		$data["getData"] = $this->servicesModel->get_Electrical();
+		$this->load->view('templates/head');
+		$this->load->view('templates/navbar');
+		$this->load->view("electricalIndex",$data);
+		$this->load->view('templates/footer');
+	}
+
+	public function showElectricalDetail($electricalID){
+		$data = array(
+			'headtitle' => 'Body',
+		);
+		$data["getDataByID"] = $this->servicesModelce->get_Data_by_ID($electricalID);
+		// $this->load->view('templates/head');
+		// $this->load->view('templates/navbar');
+		$this->load->view("detailIndex",$data);
+		// $this->load->view('templates/footer');
+	}
+
+//engine index
+	public function showEngine(){
+		$data = array(
+			'headtitle' => 'Body',
+		);
+		$data["getData"] = $this->servicesModel->get_Engine();
+		$this->load->view('templates/head');
+		$this->load->view('templates/navbar');
+		$this->load->view("engineIndex",$data);
+		$this->load->view('templates/footer');
+	}
+
+	public function showEngineDetail($engineID){
+		$data = array(
+			'headtitle' => 'Body',
+		);
+		$data["getDataByID"] = $this->servicesModel->get_Data_by_ID($engineID);
+		// $this->load->view('templates/head');
+		// $this->load->view('templates/navbar');
+		$this->load->view("detailIndex",$data);
+		// $this->load->view('templates/footer');
 	}
 }
 ?>
--- application/models/servicesModel.php
+++ application/models/servicesModel.php
@@ -1,34 +1,83 @@
 <?php
 class servicesModel extends CI_Model{
 
+//global
+	function get_Data_by_ID($getID){
+        $this->db->select('*');
+        $this->db->where('ID',$getID);
+        $query = $this->db->get("service");
+        $data = $query->result_array();
+        return $data;
+    }
 
-	 public function delete_Data_by_ID($deleteID){
+	function delete_Data_by_ID($deleteID){
     	$this->db->where("ID",$deleteID);
-    	$this->db->delete("users");
+    	$this->db->delete("service");
     }
 
     function insert_Data($data){
-    	$this->db->insert("users",$data);
+    	$this->db->insert("service",$data);
     }
 
     function update_Data_by_ID($updateID,$data){
     	$this->db->where('ID',$updateID);
-    	$this->db->update("users",$data);
+    	$this->db->update("service",$data);
     }
 
     function get_all_pics(){
-		$getPics = $this->db->get('users');
+		$getPics = $this->db->get('service');
 		return $getPics->result();
 	}
 
-	function save_pic($data){
-		$savePic['ID']=$data['ID'];
-		$savePic['name']=$data['name'];
-		$savePic['fullName']=$data['fullName'];
-		$savePic['email']=$data['email'];
-		$savePic['password']=$data['password'];
-		$savePic['pictures']=$data['pictures'];
-		$query=$this->db->insert('users',$savePic);
+//body
+	function get_Body(){
+		$this->db->select('*');
+		$this->db->where_in("SELECT * FROM service WHERE tag = 'Body'");
+        $this->db->where('tag =','Body'); 
+		$query = $this->db->get("service");
+		$data = $query->result_array();
+		return $data;
+	}
+
+//cr
+	function get_CarRescue(){
+		$this->db->select('*');
+		$this->db->where_in("SELECT * FROM service WHERE tag = 'Car Rescue'");
+        $this->db->where('tag =','Car Rescue'); 
+		$query = $this->db->get("service");
+		$data = $query->result_array();
+		return $data;
+	}
+
+//electrical
+	function get_Electrical(){
+		$this->db->select('*');
+		$this->db->where_in("SELECT * FROM service WHERE tag = 'Electrical'");
+        $this->db->where('tag =','Electrical'); 
+		$query = $this->db->get("service");
+		$data = $query->result_array();
+		return $data;
+	}
+
+//engine
+	function get_Engine(){
+		$this->db->select('*');
+		$this->db->where_in("SELECT * FROM service WHERE tag = 'Engine'");
+        $this->db->where('tag =','Engine'); 
+		$query = $this->db->get("service");
+		$data = $query->result_array();
+		return $data;
+	}
+
+//d2d
+	function get_D2D(){
+		$this->db->select('*');
+		$this->db->where_in("SELECT * FROM service WHERE tag = 'Door-to-Door'");
+        $this->db->where('tag =','Door-to-Door'); 
+		$query = $this->db->get("service");
+		$data = $query->result_array();
+		return $data;
 	}
 }
+
 ?>
