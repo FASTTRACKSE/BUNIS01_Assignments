1e1ee29c997b3ceadc758cc98451bb9e79efbbc1
Angeliinee <Angeliinee@users.noreply.github.com>
2018-03-30 21:13:52 +0700

    .

--- application/views/adminpages/welcome_admin.php
+++ application/views/adminpages/welcome_admin.php
@@ -10,7 +10,7 @@
       </ol>
       <div class="row">
         <div class="col-12">      
-         <img src="<?php echo base_url().'/assets/image/bg.jpg'?>" style="height: 100%; width: 100%; object-fit: contain" >
+         <img src="<?php echo base_url().'/assets/image/bg.jpg'?>" style="height: 100%; width: 100%; object-fit: contain; border-radius: 8px;" >
           
         </div>
       </div>
