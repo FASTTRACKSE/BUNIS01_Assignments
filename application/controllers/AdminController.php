02c91aa60876d05bf5d577147ccc386c3c9a04c6
hendrixsilaen <hendrixsilaen@users.noreply.github.com>
2018-03-30 10:35:45 +0700

    login

--- application/controllers/AdminController.php
+++ application/controllers/AdminController.php
@@ -12,33 +12,9 @@
 			$this->load->helper("url");
 
 			if (!$this->session->userdata('username')) {
-		        $this->load->view('adminpages/login');
+		        redirect('LoginController/login');
 		    }
 		}
-
-		public function logout(){
-			$this->session->sess_destroy();
-			redirect(base_url().'index.php/StudentController/');
-		}
-
-		public function loginValidation(){
-
-		$username = $this->input->post('username');
-		$password = md5($this->input->post('password'));
-
-		if($this->studentmodel->checkLogin($username, $password)){
-
-			$session_data = array(
-				'username' => $username
-			);
-			$this->session->set_userdata($session_data);
-			redirect(base_url().'index.php/StudentController/index');
-		}
-		else{
-			$this->session->set_flashdata('error', 'Invalid Username and Password');  
-            
-		}
-	}
 
 		public function companyProfile()
 		{
