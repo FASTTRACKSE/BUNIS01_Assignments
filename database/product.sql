26dfbb1bf20cc97986ea3b934a016dc02bb005a9
beni12344 <37788937+beni12344@users.noreply.github.com>
2018-04-03 15:07:02 +0700

    a

--- database/product.sql
+++ database/product.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 26, 2018 at 06:52 AM
+-- Generation Time: Apr 03, 2018 at 10:06 AM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -25,156 +25,53 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `accessory`
+-- Table structure for table `product`
 --
 
-CREATE TABLE `accessory` (
-  `AccessoryID` int(255) NOT NULL,
-  `AccessoryName` varchar(255) NOT NULL,
-  `AccessoryPrice` int(255) NOT NULL,
-  `AccessoryDesc` varchar(255) NOT NULL,
-  `AccessoryImage` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `carparts`
---
-
-CREATE TABLE `carparts` (
-  `CarPartsID` int(255) NOT NULL,
-  `CarPartsName` varchar(255) NOT NULL,
-  `CarPartsPrice` int(255) NOT NULL,
-  `CarPartsDesc` varchar(255) NOT NULL,
-  `CarPartsImage` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `insurance`
---
-
-CREATE TABLE `insurance` (
-  `InsuranceID` int(255) NOT NULL,
-  `InsuranceName` varchar(255) NOT NULL,
-  `InsurancePrice` int(255) NOT NULL,
-  `InsuranceDesc` varchar(255) NOT NULL,
-  `InsuranceImage` varchar(255) NOT NULL
+CREATE TABLE `product` (
+  `ProductID` int(255) NOT NULL,
+  `ProductName` varchar(255) NOT NULL,
+  `ProductPrice` varchar(255) NOT NULL,
+  `ProductDesc` varchar(255) NOT NULL,
+  `ProductType` varchar(255) NOT NULL,
+  `ProductImage` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `insurance`
+-- Dumping data for table `product`
 --
 
-INSERT INTO `insurance` (`InsuranceID`, `InsuranceName`, `InsurancePrice`, `InsuranceDesc`, `InsuranceImage`) VALUES
-(1, 'Allstate', 5000000, 'AllState Car Insurance', 'allstate.jpg'),
-(2, 'Progressive', 1000000, 'Progressive Car Insurance', 'progressive.jpg'),
-(3, 'Liberty Mutual', 3000000, 'Liberty Mutual Car Insurance', 'libertymutual.jpg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `oilandfluid`
---
-
-CREATE TABLE `oilandfluid` (
-  `OilandFluidID` int(255) NOT NULL,
-  `OilandFluidName` varchar(255) NOT NULL,
-  `OilandFluidPrice` int(255) NOT NULL,
-  `OilandFluidDesc` varchar(255) NOT NULL,
-  `OilandFluidImage` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `usedcars`
---
-
-CREATE TABLE `usedcars` (
-  `UsedCarsID` int(255) NOT NULL,
-  `UsedCarsName` varchar(255) NOT NULL,
-  `UsedCarsPrice` int(255) NOT NULL,
-  `UsedCarsDesc` varchar(255) NOT NULL,
-  `UsedCarsImage` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `usedcars`
---
-
-INSERT INTO `usedcars` (`UsedCarsID`, `UsedCarsName`, `UsedCarsPrice`, `UsedCarsDesc`, `UsedCarsImage`) VALUES
-(1, 'UsedCar1', 5000, 'UsedCar1', 'usedcars1.jpg');
+INSERT INTO `product` (`ProductID`, `ProductName`, `ProductPrice`, `ProductDesc`, `ProductType`, `ProductImage`) VALUES
+(1, 'Allstate', '100', 'Insurance', 'Insurance', 'allstate.jpg'),
+(2, 'Progressive', '200', 'Insurance', 'Insurance', 'allstate.jpg'),
+(3, 'Steering Wheel', '100', 'CarParts', 'CarParts', 'allstate.jpg'),
+(4, 'Car Mirror', '200', 'CarParts', 'CarParts', 'allstate.jpg'),
+(5, 'Car Seat', '100', 'Accessory', 'Accessory', 'allstate.jpg'),
+(6, 'Charger', '200', 'Accessory', 'Accessory', 'allstate.jpg'),
+(7, 'Castrol', '100', 'Oil and Fluid', 'OilandFluid', 'allstate.jpg'),
+(8, 'Shell', '200', 'Oil and Fluid', 'OilandFluid', 'allstate.jpg'),
+(9, 'Usedcar1', '100', 'Usedcar1', 'UsedCars', 'allstate.jpg'),
+(10, 'Usedcar2', '200', 'aaaa', 'UsedCars', 'Usedcar2.jpg');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `accessory`
+-- Indexes for table `product`
 --
-ALTER TABLE `accessory`
-  ADD PRIMARY KEY (`AccessoryID`);
-
---
--- Indexes for table `carparts`
---
-ALTER TABLE `carparts`
-  ADD PRIMARY KEY (`CarPartsID`);
-
---
--- Indexes for table `insurance`
---
-ALTER TABLE `insurance`
-  ADD PRIMARY KEY (`InsuranceID`);
-
---
--- Indexes for table `oilandfluid`
---
-ALTER TABLE `oilandfluid`
-  ADD PRIMARY KEY (`OilandFluidID`);
-
---
--- Indexes for table `usedcars`
---
-ALTER TABLE `usedcars`
-  ADD PRIMARY KEY (`UsedCarsID`);
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`ProductID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `accessory`
+-- AUTO_INCREMENT for table `product`
 --
-ALTER TABLE `accessory`
-  MODIFY `AccessoryID` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `carparts`
---
-ALTER TABLE `carparts`
-  MODIFY `CarPartsID` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `insurance`
---
-ALTER TABLE `insurance`
-  MODIFY `InsuranceID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `oilandfluid`
---
-ALTER TABLE `oilandfluid`
-  MODIFY `OilandFluidID` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `usedcars`
---
-ALTER TABLE `usedcars`
-  MODIFY `UsedCarsID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `product`
+  MODIFY `ProductID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
